2f7f3cfeb7ec53a93b25497d0e8cd61ff51e636b
vitaly liber <liber@sibext.com>
2016-05-28 17:28:33 +0600

    remove uniq validates for city

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,7 +1,6 @@
 class Transfer < ActiveRecord::Base
  enum transfer_type: [:post, :necessary]
  validates :description, :from, :to, :date, :time, :transfer_type, presence: :true
- validates :from, :to, uniqueness: :true
 
  belongs_to :from, class_name: 'City', foreign_key: 'from_id'
  belongs_to :to, class_name: 'City', foreign_key: 'to_id'
