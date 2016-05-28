6401990f431ed010e1492d3acdbcd3592827e71d
vitaly liber <liber@sibext.com>
2016-05-28 15:37:51 +0600

    added gem Entity and transfer list to api

--- app/api/transferit/v1/transfers.rb
+++ app/api/transferit/v1/transfers.rb
@@ -1,5 +1,28 @@
 module Transferit
   module V1
+    module Entities
+      class Languages < Grape::Entity
+        expose :title
+        expose :code
+      end
+
+      class Transfers < Grape::Entity
+
+        expose :transfer do
+          expose :description
+          expose :from
+          expose :to
+          expose :date
+          expose :time do |model|
+            model.time.strftime('%H:%M')
+          end
+          expose :user_id
+          expose :transfer_type
+        end
+
+      end
+    end
+
     class Transfers < Grape::API
       format :json
 
@@ -10,11 +33,17 @@ module Transferit
           requires :from, type: String, default: 'Moscow', desc: 'Transfers from'
           requires :to, type: String, default: 'Novosibirsk', desc: 'Transfers to'
           optional :date, type: String, default: Time.now.to_date, desc: 'Transfers date'
-          optional :type, type: Symbol, default: :send, values: [:send, :necessary], desc: 'Transfers type'
+          optional :type, type: Symbol, default: :post, values: [:post, :necessary], desc: 'Transfers type'
         end
 
         post do
-          {transfers: {hello: 'wow'}}
+          transfers = Transfer.where(transfer_type: Transfer.transfer_types[ params[:type] ],
+                                     from: params[:from],
+                                     to: params[:to],
+                                     date: params[:date])
+
+          present transfers, with: Transferit::V1::Entities::Transfers, root: 'transfers'
+
         end
 
       end
