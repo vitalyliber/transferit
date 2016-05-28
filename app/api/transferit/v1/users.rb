5d1bb35e2547af03640c1eae1c941458c6f10e10
vitaly liber <liber@sibext.com>
2016-05-28 22:51:57 +0600

    added register or auth method

--- app/api/transferit/v1/users.rb
+++ app/api/transferit/v1/users.rb
@@ -8,9 +8,16 @@ module Transferit
         desc 'Register user or authorization'
         params do
           requires :phone, type: Integer, default: 89008003344, desc: 'User phone number'
+          requires :first_name, type: String, default: 'Banana', desc: 'First name for user'
+          requires :last_name, type: String, default: 'Rich', desc: 'Last name for user'
         end
 
-        post do
+        post 'auth' do
+          user = MobileUser.find_or_create_by(phone: params[:phone])
+
+          user.update(first_name: params[:first_name], last_name: params[:last_name]) if user.new_record?
+
+          {user_id: user.id}
         end
 
       end
