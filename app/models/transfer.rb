5d6cb95217fe09bba64a20dc69a8506cba500eb5
vitaly liber <liber@sibext.com>
2016-05-28 18:27:20 +0600

    added user to transfers

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -4,4 +4,5 @@ class Transfer < ActiveRecord::Base
 
   belongs_to :from, class_name: 'City', foreign_key: 'from_id'
   belongs_to :to, class_name: 'City', foreign_key: 'to_id'
+  belongs_to :user, dependent: :destroy
 end
