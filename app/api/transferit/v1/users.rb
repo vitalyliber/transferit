2ea0f05c5e075d39ef2a3ad5de08b134fd9cb9df
vitaly liber <liber@sibext.com>
2016-05-29 00:56:12 +0600

    changed auth method

--- app/api/transferit/v1/users.rb
+++ app/api/transferit/v1/users.rb
@@ -8,16 +8,26 @@ module Transferit
         desc 'Register user or authorization'
         params do
           requires :phone, type: Integer, default: 89008003344, desc: 'User phone number'
-          requires :first_name, type: String, default: 'Banana', desc: 'First name for user'
-          requires :last_name, type: String, default: 'Rich', desc: 'Last name for user'
         end
 
         post 'auth' do
           user = MobileUser.find_or_create_by(phone: params[:phone])
 
-          user.update(first_name: params[:first_name], last_name: params[:last_name]) if user.new_record?
-
           {user_id: user.id}
+        end
+
+        params do
+          requires :first_name, type: String, desc: 'First name for user'
+          requires :last_name, type: String, desc: 'Last name for user'
+          requires :user_id, type: Integer, desc: 'User id'
+        end
+
+        post 'update_profile' do
+          user = MobileUser.find_by_id(params[:user_id])
+
+          user.update(first_name: params[:first_name], last_name: params[:last_name])
+
+          {user_id: user.id, first_name: user.first_name, last_name: user.last_name}
         end
 
       end
--- app/models/mobile_user.rb
+++ app/models/mobile_user.rb
@@ -1,3 +1,3 @@
 class MobileUser < ActiveRecord::Base
-  validates :phone, :first_name, :last_name, presence: :true
+  validates :phone, presence: :true
 end
