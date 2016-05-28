447aece7f2c13ed5ca71a9f51b2445896cd3c959
vitaly liber <liber@sibext.com>
2016-05-28 15:48:52 +0600

    added model City

---
+++ app/models/city.rb
@@ -0,0 +1,3 @@
+class City < ActiveRecord::Base
+  validates :title, presence: :true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528083225) do
+ActiveRecord::Schema.define(version: 20160528094038) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "transfers", force: :cascade do |t|
     t.text     "description"
