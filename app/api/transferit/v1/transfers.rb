4c872639642be9ec3d93a00f80047c7d6fda8ee1
vitaly liber <liber@sibext.com>
2016-05-28 18:54:42 +0600

    added parcels method to api

--- app/api/transferit/v1/transfers.rb
+++ app/api/transferit/v1/transfers.rb
@@ -1,9 +1,14 @@
 module Transferit
   module V1
     module Entities
-      class Languages < Grape::Entity
-        expose :title
-        expose :code
+      class Parcels < Grape::Entity
+        expose :parcel do
+          expose :description
+          expose :from_id
+          expose :to_id
+          expose :date
+          expose :user_id
+        end
       end
 
       class Transfers < Grape::Entity
@@ -40,6 +45,23 @@ module Transferit
                                      date: params[:date])
 
           present transfers, with: Transferit::V1::Entities::Transfers, root: 'transfers'
+        end
+
+        desc 'Get Parcels'
+        params do
+          requires :from, type: Integer, default: 1, desc: 'City id'
+          requires :to, type: Integer, default: 2, desc: 'City id'
+          optional :date, type: String, default: Time.now.to_date, desc: 'Transfers date'
+        end
+
+        post 'parcels' do
+          parcels = Parcel.where(from: params[:from],
+                                     to: params[:to],
+                                     date: params[:date])
+
+          parcels = parcels.where(date: params[:date]) if params[:date].present?
+
+          present parcels, with: Transferit::V1::Entities::Parcels, root: 'parcels'
         end
 
       end
